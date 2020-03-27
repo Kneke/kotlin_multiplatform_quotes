9511d9643ea74c5a524df5cdf3b1a865c4edc112
Christoph Knetschke <christoph.knetschke@daimler.com>
2020-03-27 17:45:35 +0100

    Updates Libraries to newest version which works with kotlin 1.3.60
    - only sqldelight needs a new version to do the update to 1.3.70

--- common/src/iosMain/kotlin/de/kneke/common/util/db/DatabaseHelper.kt
+++ common/src/iosMain/kotlin/de/kneke/common/util/db/DatabaseHelper.kt
@@ -1,7 +1,7 @@
 package de.kneke.common.util.db
 
 import com.squareup.sqldelight.db.SqlDriver
-import com.squareup.sqldelight.drivers.ios.NativeSqliteDriver
+import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
 import de.kneke.common.db.MyDatabase
 
 actual fun getDriver(dbName: String): SqlDriver = NativeSqliteDriver(MyDatabase.Schema, dbName)
