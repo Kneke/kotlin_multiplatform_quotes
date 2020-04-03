8d09551ac7d4477b2ce209a879543b83885cfb47
Christoph Knetschke <christoph.knetschke@daimler.com>
2020-04-03 11:04:21 +0200

    Combine common module with flutter
    - not fully work android project

--- flutterapp/android/app/src/main/kotlin/de/kneke/flutterapp/App.kt
+++ flutterapp/android/app/src/main/kotlin/de/kneke/flutterapp/App.kt
@@ -3,11 +3,14 @@ package de.kneke.flutterapp
 import io.flutter.app.FlutterApplication
 import android.content.Context
 import androidx.multidex.MultiDex
+import de.kneke.common.util.libContext
 
 class App : FlutterApplication() {
 
     override fun attachBaseContext(base: Context) {
         super.attachBaseContext(base)
         MultiDex.install(this)
+
+        libContext = base
     }
 }
